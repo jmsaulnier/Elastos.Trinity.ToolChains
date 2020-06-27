d4093b32404808362f1e80f097268493e716e2de
Chad <chadracelis@yahoo.com>
2020-06-27 14:10:13 -0400

    Update titlebar guide and demos

--- cli/assets/dapptemplate/angular/demo/src/app/pages/titlebar/titlebar-demo/titlebar-demo.page.ts
+++ cli/assets/dapptemplate/angular/demo/src/app/pages/titlebar/titlebar-demo/titlebar-demo.page.ts
@@ -11,7 +11,9 @@ declare let titleBarManager: TitleBarPlugin.TitleBarManager;
 })
 export class TitlebarDemoPage implements OnInit {
 
-  public manager;
+  public manager: any;
+  public titleToggled: boolean = false;
+  public colorToggled: boolean = false;
 
   constructor(
     private navParams: NavParams,
@@ -21,6 +23,22 @@ export class TitlebarDemoPage implements OnInit {
   ngOnInit() {
     this.manager = this.navParams.get('manager');
     console.log('Titlebar Example', this.manager);
+
+    if(this.manager.type === 'Icons') {
+      this.titlebarService.setIcon();
+    }
+    if(this.manager.type === 'Items') {
+      this.titlebarService.setMenuItems();
+      titleBarManager.addOnItemClickedListener(this.titlebarService.onItemClickedListener = (menuIcon) => {
+        if (menuIcon.key === "help") {
+          console.log("Help item clicked");
+          this.titlebarService.alertTitlebarItemClicked(
+            'You clicked a Titlebar item!', 
+            'This action won\'t do anything but demonstrate the Titlebar item.'
+          );
+        }
+      });
+    }
   }
 
   ionViewWillEnter() {
@@ -29,8 +47,28 @@ export class TitlebarDemoPage implements OnInit {
   }
 
   ionViewWillLeave() {
-    titleBarManager.setTitle("Demo Template");
     this.titlebarService.setTitleBarBackKeyShown(false);
   }
 
+  toggleTitle() {
+    if(this.titleToggled) {
+      this.titleToggled = false;
+      titleBarManager.setTitle('Titlebar Demo');
+    } else {
+      this.titleToggled = true;
+      titleBarManager.setTitle('Titlebar Changed!');
+    }
+  }
+
+  toggleColor() {
+    if(this.colorToggled) {
+      this.colorToggled = false;
+      titleBarManager.setBackgroundColor("#181d20");
+      titleBarManager.setForegroundMode(TitleBarPlugin.TitleBarForegroundMode.LIGHT);
+    } else {
+      this.colorToggled = true;
+      titleBarManager.setBackgroundColor("#ffffff");
+      titleBarManager.setForegroundMode(TitleBarPlugin.TitleBarForegroundMode.DARK);
+    }
+  }
 }
--- cli/assets/dapptemplate/angular/demo/src/app/services/titlebar.service.ts
+++ cli/assets/dapptemplate/angular/demo/src/app/services/titlebar.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { ModalController, NavController } from '@ionic/angular';
+import { ModalController, NavController, AlertController } from '@ionic/angular';
 import { TitlebarDemoPage } from '../pages/titlebar/titlebar-demo/titlebar-demo.page';
 import { Router } from '@angular/router';
 
+declare let appManager: AppManagerPlugin.AppManager;
 declare let titleBarManager: TitleBarPlugin.TitleBarManager;
 
 @Injectable({
@@ -15,7 +16,7 @@ export class TitlebarService {
       type: 'Title',
       title: 'Customize Title',
       method: 'setTitle',
-      message: 'The title of your app can be changed whenever and wherever. This can be useful to display the title of your page, the occuring action or anything you desire.',
+      message: 'The title of your app can be changed at any time. This can be useful to display the title of your page, the occuring action or anything you desire.',
       message2: null,
       example: 'assets/titlebar/title.png'
     },
@@ -23,7 +24,7 @@ export class TitlebarService {
       type: 'Color',
       title: 'Customize Color',
       method: 'setBackgroundColor/setForegroundMode',
-      message: 'At any time, you can customize the background and title color of your titlebar to match the theme of your app.',
+      message: 'At any time, you can customize the background and title color of your Titlebar to match the theme of your app.',
       message2: null,
       example: 'assets/titlebar/color.png'
     },
@@ -31,17 +32,18 @@ export class TitlebarService {
       type: 'Icons',
       title: 'Customize Icons',
       method: 'setIcon',
-      message: 'Setting an icon is a special way add buttons to your titlebar. To set an icon, make sure to have a listener ready once your app is rendered. Then you can add your icon to a page where you want it revealed using the page\'s life cycle.',
+      message: 'Setting an icon is a special way add buttons to your titlebar.',
+      message2: 'To set an icon, make sure to have a listener ready once your app or page is rendered. Then you can add your icon to a page where you want it revealed using the page\'s life cycle.',
       message3: 'Make sure to declare the position of the icon, a key (special name to handle the icon) and the icon itself which you can set the path for or simply use one of elastOS built-in icons.',
-      example2: 'assets/titlebar/listener.png',
-      example3: 'assets/titlebar/setIcon.png',
+      example: 'assets/titlebar/listener.png',
+      example2: 'assets/titlebar/setIcon.png',
     },
     {
       type: 'Navigation',
       title: 'Customize Navigation',
       method: 'setIcon',
-      message: 'Setting your app\'s navigation is similar to setting an icon to your titlebar.',
-      message2: 'To set naviation in your titlebar, make sure to have a listener ready once your app is rendered. Then you can add (or) remove the elastOS back-icon in a page using a life cycle.',
+      message: 'Setting your app\'s navigation is similar to setting an icon to your Titlebar.',
+      message2: 'To set naviation in your Titlebar, make sure to have a listener ready once your app or page is rendered. Then you can add (or) remove the elastOS back-icon in a page using a life cycle.',
       message3: 'Make sure to declare the position of the icon (ideally in the outer left of the titlebar), a key (special name to handle the icon) and the icon itself.',
       example: 'assets/titlebar/back-listener.png',
       example2: 'assets/titlebar/setBackIcon.png',
@@ -51,11 +53,15 @@ export class TitlebarService {
       type: 'Items',
       title: 'Customize Items',
       method: 'setupMenuItems',
-      message: 'One of the best ways to customize your titlebar is to add menu items in it. Declaring this will add an options key to the right corner of your titlebar that displays a list of menu items you provided.',
-      message2: 'This will give you the ability to add a list of actions with a custom icon in your title bar which can trigger a callback you have set for it.',
-      example: 'assets/titlebar/items.png'
+      message: 'Similar to setting icons to your Titlebar, setting up menu items will present an options key to display a list of menu items you provided. This is an alternative option for adding buttons to your Titlebar without cluttering it.',
+      message2: 'Like always, make sure to have a Titlebar listener prepared to handle your menu items using the special key you provided. As well as a path to your icon and a title to visually present this action.',
+      message3: 'Depending on the state of your app, there are cases you may want to remove your Titlebar items. To do this, make sure to store your menu item as a variable. You can then add it under your listener or remove it under a page\'s life cycle.',
+      example: 'assets/titlebar/items.png',
+      example2: 'assets/titlebar/removeItems.png',
     }
   ];
+
+  public onItemClickedListener: any;
 
   async openManager(manager: any) {
     const modal = await this.modalCtrl.create({
@@ -68,17 +74,41 @@ export class TitlebarService {
     modal.present();
   }
 
-  constructor(private modalCtrl: ModalController, private router: Router) { }
+  constructor(
+    private modalCtrl: ModalController, 
+    private navCtrl: NavController,
+    private alertCtrl: AlertController
+  ) { }
 
   init() {
     this.setColor();
 
+    // Listener for titlebar icons
     titleBarManager.addOnItemClickedListener((menuIcon) => {
       if (menuIcon.key === "back") {
         this.modalCtrl.dismiss();
-        this.router.navigate(['/home']);
+        this.navCtrl.navigateBack(['/home']);
+
+        titleBarManager.setTitle("Demo Template");
+        titleBarManager.setBackgroundColor("#181d20");
+        titleBarManager.setForegroundMode(TitleBarPlugin.TitleBarForegroundMode.LIGHT);
+        titleBarManager.setIcon(TitleBarPlugin.TitleBarIconSlot.INNER_RIGHT, null);
+        titleBarManager.removeOnItemClickedListener(this.onItemClickedListener);
       }
+      this.onTitlebarItemClicked(menuIcon);
     });
+  }
+
+  onTitlebarItemClicked(icon: TitleBarPlugin.TitleBarIcon) {
+    switch (icon.key) {
+      case 'add':
+        console.log('Add icon clicked');
+        this.alertTitlebarItemClicked(
+          'You clicked a Titlebar icon!', 
+          'This action won\'t do anything but demonstrate the Titlebar icon.'
+        );
+        break;
+    }
   }
 
   /******************** Set Title ********************/
@@ -110,22 +140,46 @@ export class TitlebarService {
   /******************** Set and Handle Icons ********************/
   setIcon() {
     titleBarManager.setIcon(TitleBarPlugin.TitleBarIconSlot.OUTER_RIGHT, {
+      // Special key to handle item
       key: "add",
+      // Add a path to your own icon or use one of elastOS built-in icons
       iconPath:  TitleBarPlugin.BuiltInIcon.ADD
     });
   }
 
-  /******************** Set Menu Items ********************/
+  /******************** Setup Menu Items ********************/
   setMenuItems() {
- /*    titleBarManager.setupMenuItems(
+    titleBarManager.setupMenuItems(
       [
         {
-          key: "", // Add uniqute item key
-          iconPath: "", // Add path to item icon
-          title: "" // Add item title
+          // Special key to handle item
+          key: "help",
+          // Add path to your icon
+          iconPath: TitleBarPlugin.BuiltInIcon.HELP,
+          // Add title of your item
+          title: "Test Menu Item"
         }
       ],
-    ); */
+    );
+  }
+
+  /******************* Simple alerts to demonstrate titlebar icons and items *******************/
+  async alertTitlebarItemClicked(header: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      mode: "ios",
+      header: header,
+      message: message,
+      buttons: [
+        {
+          text: "Cool!",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+          }
+        },
+      ]
+    });
+    alert.present();
   }
 
 }
