6eaaa85e075b3bff18e97be02ef865dbc5fac315
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-10-12 14:21:32 +0800

    CU-aeg99w Fixed dapp package id different from native package id + remove specific elastOS intent filters from manifest

--- lib/ela_util.py
+++ lib/ela_util.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import distutils.dir_util as dir_util
 
 # Removes a path if it exists, but don't throw an error if it doesn't
@@ -14,7 +15,7 @@ def safeRemove(path):
     # Make sure the deletion was really successful (no file system lock):
     if os.path.exists(path):
         print("Error: file/folder could not be totally deleted. Check possible file system lock: "+path)
-        os.exit(1)
+        sys.exit(1)
 
 # Create a folder if it does not exists, but don't throw an error if it already exists
 def safeMkdir(path):
